cb1aad70564cf0857d4c3082ab9fa5ee1bb6af2d
Dongmin Yu <douglas-catyu-gm@outlook.com>
2026-05-06 15:54:19 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
-    base44({
+    //base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
       // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
+      //legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      //hmrNotifier: true,
+      //navigationNotifier: true,
+      //analyticsTracker: true,
+     //visualEditAgent: true
+    //}),
     react(),
   ]
 });
