d4d1e0693f84869adc5e63987690f0e87c62696b
Dongmin Yu <douglas-catyu-gm@outlook.com>
2026-05-06 16:39:04 +0800

    l

--- src/components/ui/chart.jsx
+++ src/components/ui/chart.jsx
@@ -55,12 +55,13 @@ const ChartStyle = ({
   if (!colorConfig.length) {
     return null
   }
-
+/* *
   return (
     (<style
       dangerouslySetInnerHTML={{
         __html: Object.entries(THEMES)
           .map(([theme, prefix]) => `
+**/
 ${prefix} [data-chart=${id}] {
 ${colorConfig
 .map(([key, itemConfig]) => {
